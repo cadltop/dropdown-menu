a7be0795b7a33ccb11d87345031a12d0f2923a91
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-24 00:50:46 -0500

    Lint and format code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import 'normalize.css';
-import './index.css';
+import "normalize.css";
+import "./index.css";
 
-const icon = document.querySelector('img');
-const menuDiv = document.querySelector('div.menu');
+const icon = document.querySelector("img");
+const menuDiv = document.querySelector("div.menu");
 
 hideMenu();
 
 icon.onclick = showMenu;
 menuDiv.onmouseleave = hideMenu;
 
-function hideMenu() {menuDiv.style.display = 'none';}
-function showMenu() {menuDiv.style.display = 'flex';}
+function hideMenu() {
+  menuDiv.style.display = "none";
+}
+function showMenu() {
+  menuDiv.style.display = "flex";
+}
