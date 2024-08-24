a53f5f8b23d2c7c98caa4114997a64a5566744aa
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-24 00:46:42 -0500

    Show menu on click of icon and hide when leaving menuDiv

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import 'normalize.css';
 import './index.css';
+
+const icon = document.querySelector('img');
+const menuDiv = document.querySelector('div.menu');
+
+hideMenu();
+
+icon.onclick = showMenu;
+menuDiv.onmouseleave = hideMenu;
+
+function hideMenu() {menuDiv.style.display = 'none';}
+function showMenu() {menuDiv.style.display = 'flex';}
